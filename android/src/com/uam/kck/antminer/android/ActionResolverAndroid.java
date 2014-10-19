a13ec2c0bee529e1b65eb32fbf8e62304a5edc96
Hubert <hubert.dworczynski@gmail.com>
2014-10-20 00:29:27 +0200

    Speech popup displaying correctly now. Can't figure out how to get the spoken text back from GetActivityForResult().

--- android/src/com/uam/kck/antminer/android/ActionResolverAndroid.java
+++ android/src/com/uam/kck/antminer/android/ActionResolverAndroid.java
@@ -1,11 +1,8 @@
 package com.uam.kck.antminer.android;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.widget.Toast;
--- android/src/com/uam/kck/antminer/android/AndroidLauncher.java
+++ android/src/com/uam/kck/antminer/android/AndroidLauncher.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.uam.kck.antminer.AntMinerGame;
+import com.uam.kck.antminer.SpeechTest;
 
 public class AndroidLauncher extends AndroidApplication {
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new AntMinerGame(), config);
-	}
+    ActionResolverAndroid actionResolver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        actionResolver = new ActionResolverAndroid(this);
+
+        initialize(new SpeechTest(actionResolver));
+    }
 }
