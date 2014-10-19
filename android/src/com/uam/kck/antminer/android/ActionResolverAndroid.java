3e769ff77938ddeac5f0de45c8c941792a94c89d
Hubert <hubert.dworczynski@gmail.com>
2014-10-20 00:23:04 +0200

    Speech popup displaying correctly now. Can't figure out how to get the spoken text back from GetActivityForResult().

--- android/src/com/uam/kck/antminer/android/ActionResolverAndroid.java
+++ android/src/com/uam/kck/antminer/android/ActionResolverAndroid.java
@@ -13,13 +13,11 @@ import android.widget.Toast;
 import com.badlogic.gdx.Gdx;
 import com.uam.kck.antminer.ActionResolver;
 
-import java.util.ArrayList;
-
 
 /**
  * Created by hubert on 19.10.14.
  */
-public class ActionResolverAndroid extends Activity implements ActionResolver {
+public class ActionResolverAndroid implements ActionResolver {
     public static final int REQUEST_OK = 1;
 
     Handler uiThread;
@@ -41,55 +39,30 @@ public class ActionResolverAndroid extends Activity implements ActionResolver {
     }
 
     @Override
-    public void showAlertBox(final String alertBoxTitle, final String alertBoxMessage, final String alertBoxButtonText) {
+    public void showSpeechPopup() {
         uiThread.post(new Runnable() {
             @Override
             public void run() {
-                new AlertDialog.Builder(appContext)
-                        .setTitle(alertBoxTitle)
-                        .setMessage(alertBoxMessage)
-                        .setNeutralButton(alertBoxButtonText,
-                                new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                    }
-                                }).create().show();
+                Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+                try {
+                    ((Activity)appContext).startActivityForResult(i, REQUEST_OK);
+                } catch (Exception e) {
+                    showToast(e.toString(), 5000);
+                    Gdx.app.log(ActionResolverAndroid.class.getName(), "error initializing speech engine" + e);
+                }
             }
         });
     }
 
-    @Override
-    public void openUri(String uri) {
-        Uri myUri = Uri.parse(uri);
-        Intent intent = new Intent(Intent.ACTION_VIEW, myUri);
-        appContext.startActivity(intent);
-    }
-
-    @Override
-    public void showMyList() {
-        appContext.startActivity(new Intent(this.appContext, MyListActivity.class));
-    }
-
-    @Override
-    public void showSpeechPopup() {
-        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-        try {
-            startActivityForResult(i, REQUEST_OK);
-        } catch (Exception e) {
-            showToast(e.toString(), 5000);
-            Gdx.app.log(ActionResolverAndroid.class.getName(), "error initializing speech engine" + e);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_OK && resultCode == RESULT_OK) {
-            ArrayList<String> thingsYouSaid =
-                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-
-            //TODO: do something with the text
-        }
-    }
+    //protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    //    super.onActivityResult(requestCode, resultCode, data);
+    //    if (requestCode == REQUEST_OK && resultCode == RESULT_OK) {
+    //        ArrayList<String> thingsYouSaid =
+    //                data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+    //
+    //        //TODO: do something with the text
+    //        showToast(thingsYouSaid.get(0), 5000);
+    //    }
+    //}
 }
--- core/src/com/uam/kck/antminer/ActionResolver.java
+++ core/src/com/uam/kck/antminer/ActionResolver.java
@@ -5,10 +5,5 @@ package com.uam.kck.antminer;
  */
 public interface ActionResolver {
     public void showToast(CharSequence toastMessage, int toastDuration);
-    public void showAlertBox(String alertBoxTitle, String alertBoxMessage,
-                             String alertBoxButtonText);
-    public void openUri(String uri);
-    public void showMyList();
-
     public void showSpeechPopup();
 }
